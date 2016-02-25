69acb1c2be33f916f24bfa2dc2ff780ba96b0175
Eugenio Leal <eugenio.leal.333@gmail.com>
2016-02-24 20:13:09 -0600

    full game with customized Pickups

---
+++ Assets/Scripts/CamaraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamaraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;//get the distance between the camara and player(ball) for third person view
+
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	// Since we dont use forces void Updade is used instead of void FixedUpdate
+	void Update () {
+        /*delta Time is to get The time in seconds it took to complete the last frame (Read Only).
+        to make your game frame rate independent.*/
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+	}
+}
